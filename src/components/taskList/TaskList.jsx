c0c2e4c053ab3c726c5f5d312a2341389affdba3
hasan <hasanakoz1986@gmail.com>
2022-10-20 21:28:45 +0300

    data fatching with axios, toggle function done

--- src/components/taskList/TaskList.jsx
+++ src/components/taskList/TaskList.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import RiDeleteBack2Fill from "react-icons/ri";
 
-const TaskList = () => {
+const TaskList = ({ task }) => {
   return (
     <div>
-      TaskList
+      {task.map((item) => {
+        const [id, task, date] = item;
+        return (
+          <div key={id}>
+            <div>
+              <h4>{task}</h4>
+              <p>{date}</p>
+            </div>
+            <div>
+              <RiDeleteBack2Fill />
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import AddTask from "../components/addTask/AddTask";
 import TaskList from "../components/taskList/TaskList";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+import { useEffect } from "react";
 
 const Home = () => {
+  const [show, setShow] = useState(false);
+  const [text, setText] = useState("Show Task Bar");
+  const [task, setTask] = useState();
+  const url = "https://63518211dfe45bbd55c21be0.mockapi.io/api/tasks";
+
+  const toggle = (e) => {
+    setShow(!show);
+    const buttonText = show ? "Show Task Bar" : "Hide Task Bar";
+    setText(buttonText);
+  };
+
+  const getTask = async () => {
+    const data = await axios(url);
+    setTask(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getTask();
+  }, []);
+
   return (
     <div>
-      Home
-      <AddTask />
-      <TaskList />
+      <Button
+        onClick={() => {
+          toggle();
+        }}
+        variant="primary"
+      >
+        {text}
+      </Button>
+      {show && <AddTask />}
+      <TaskList task={task} />
     </div>
   );
 };
